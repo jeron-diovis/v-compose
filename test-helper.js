572f8fa40713832a0af54ab493221b933896f11f
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2017-02-13 10:47:57 +0200

    setup tests

--- test-helper.js
+++ test-helper.js
@@ -1,4 +1,32 @@
 var chai = require("chai")
 var sinon = require("sinon")
+
+// ---
+
+var SinonAsPromised = require("sinon-as-promised")
+var MockPromises = require("mock-promises")
+
+// ---
+
 global.assert = chai.assert
 global.sinon = sinon
+
+// Gain full control over timers and promises.
+// Provide "tick" func to callback, allowing to manually shift time forward.
+// After each timeshift, all promises, resolved for that moment, will be executed.
+global.FakeAsync = cb => {
+  var clock = sinon.useFakeTimers()
+  global.Promise = MockPromises.getMockPromise(Promise)
+  SinonAsPromised(global.Promise)
+  try {
+    cb(ms => {
+      clock.tick(ms)
+      MockPromises.tickAllTheWay()
+    })
+  } finally {
+    MockPromises.reset()
+    global.Promise = MockPromises.getOriginalPromise()
+    SinonAsPromised(global.Promise)
+    clock.restore()
+  }
+}
